fe58068b8109f02765d3a5d6d430ce543e4e0f42
Pierre Chaminade <pierre.chaminade.code@gmail.com>
2022-08-01 18:11:17 +0200

    modif namespace

--- Tests/TestUser.php
+++ Tests/TestUser.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\User;
 use PHPUnit\Framework\TestCase;
 
 
@@ -9,7 +9,7 @@ class TestUser extends TestCase
 
  /** @test */ 
     public function TestValeurs (){
-        $user1 = new User('pierre', 25); 
+        $user = new User('pierre' , 25);
 
         $this->assertSame('pierre' , $this->name);
         $this->assertSame(25 , $this->age); 
